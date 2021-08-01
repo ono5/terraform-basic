56c09d21c93605a356ea65b3e96e7bdf334e78b8
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-08-01 14:33:59 +0900

    feat: Added docker container resource

--- main.tf
+++ main.tf
@@ -11,5 +11,15 @@ provider "docker" {}
 
 resource "docker_image" "nodered_image" {
   # docker image
-  name = "nodered/node-red]latest"
+  name = "nodered/node-red:latest"
+}
+
+resource "docker_container" "nodered_container" {
+  name = "nodered"
+  # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image#read-only
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    external = 1880
+  }
 }
