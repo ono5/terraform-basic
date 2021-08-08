7a3223aa5bec16423c9fa4808b9478eb3bb30ca9
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-08-09 04:52:34 +0900

    added count feature

--- main.tf
+++ main.tf
@@ -14,23 +14,30 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random" {
+  count   = 2 # リソースを2つ作る
+  length  = 4
+  special = false
+  upper   = false
+}
+
 resource "docker_container" "nodered_container" {
-  name = "nodered"
-  # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image#read-only
+  count = length(random_string.random) // random数分作る
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
   }
 }
 
 output "IP-Address" {
-  # value       = docker_container.nodered_container.ip_address
-  value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
+  value = docker_container.nodered_container[*].ip_address
+
   description = "The IP address of the container"
 }
 
 output "container-name" {
-  value       = docker_container.nodered_container.name
+  value       = docker_container.nodered_container[*].name
   description = "The name of the container"
 }
